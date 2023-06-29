bada19842866e232e89e75f9a8f07b5432d8a8e1
naa-7 <nalawad000@citymail.cuny.edu>
2023-06-29 16:25:49 -0400

    completed feature, redux - campuses - fetch_all_data

---
+++ src/Redux/campuses/campuses.types.js
@@ -0,0 +1,7 @@
+// Define an object to hold the campus action types
+const CampusActionType = {
+    FETCH_ALL_CAMPUSES: "FETCH_ALL_CAMPUSES",
+  };
+  
+// Export the CampusActionType object as the default export
+export default CampusActionType;
