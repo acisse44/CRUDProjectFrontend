227f6db7f90a367da8426a99f98d6fe4e60bb8f9
naa-7 <nalawad000@citymail.cuny.edu>
2023-06-29 23:14:06 -0400

    fix a small typo

--- src/Components/StudentsList.jsx
+++ src/Components/StudentsList.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 function StudentsList(props) {
+    console.log("PROPS", props.list);
     return (  
         <div>
             {props.list.map((item) => {
-                return <div>{item}</div>
+                return <div key={item.id}>{item.id} {item.firstName} {item.lastName} {item.email}</div>
             })}
         </div>
     );
