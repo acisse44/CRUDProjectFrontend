f145c1f29bd1ce3ee9ee7c23dbe134d75c9bfabd
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-01 10:48:39 -0400

    fixed some css styling ins App.css and changed some css stuff in componenets

--- src/Components/SingleStudentList.jsx
+++ src/Components/SingleStudentList.jsx
@@ -2,21 +2,45 @@ import React from "react";
 
 function SingleStudentList(props) {
   const { student } = props;
+  const campus = student.campus;
 
   return (
-    <div>
-      {Object.keys(student).length === 0 ? (
+    <div className="single-student-container">
+      {!student ? (
         <div>
           <div>Empty</div>
           <div>No student</div>
         </div>
       ) : (
         <div>
-          <div id="student" key={student.id}>
-            <img src={student.imageUrl} alt={student.firstName} id="studentImage" />
-            <div>{student.firstName} {student.lastName}</div>
+          <div id="singleStudent" key={student.id}>
+            <img src={student.imageUrl} alt={student.firstName} id="singleStudentImage" />
+            <div>
+              {student.firstName} {student.lastName}
+            </div>
+            <div>{student.gpa}</div>
             <div>{student.email}</div>
           </div>
+
+          {campus ? (
+            <h2>Campus</h2>
+          ) : (
+            <div>No campus</div>
+          )}
+
+          {campus && (
+            <div className="studentCampus">
+              <div id="studentCampus" key={campus.id}>
+                <img
+                  src={campus.imageUrl}
+                  alt={campus.Name}
+                  id="studentCampusImage"
+                />
+                <div>{campus.description}</div>
+                <div>{campus.address}</div>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
