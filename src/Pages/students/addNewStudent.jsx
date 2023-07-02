d043ee866efdbe857b6cdbf15117e10bfe5b4509
Aminata <aminata.cisse44@bcmail.cuny.edu>
2023-07-01 22:20:48 -0400

    handle functions added

--- src/Pages/students/addNewStudent.jsx
+++ src/Pages/students/addNewStudent.jsx
@@ -1,6 +1,6 @@
-/*import React, { useEffect } from 'react'
+import React, { useState } from 'react';
 
-function addNewStudent() {
+function AddNewStudent() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [imageURL, setImageURL] = useState("");
@@ -8,36 +8,41 @@ function addNewStudent() {
     const [gpa, setGpa] = useState(0);
 
 
+    const handleChange = (event) => {
+        setFirstName(event.target.value);
+        setLastName(event.target.value);
+        setImageURL(event.target.value);
+        setEmail(event.target.value);
+        setGpa(event.target.value);
+    };
+    
     return (
-
         <div>
-
             <h1> Add Student Form </h1>
-                <form>
-                    <label>
-                        First Name: 
-                            <input type = "text" value = {firstName} onChange = {this.handleChange} />
-                    </label>
-                    <label>
-                        Last Name: 
-                    <input type = "text" value = {lastName} onChange = {this.handleChange} />
-                    </label>
-                    <label>
-                    Image URL: 
-                    <input type = "text" value = {imageURL} onChange = {this.handleChange} />
-                    </label>
-                    <label>
-                    Email: 
-                    <input type = "text" value = {email} onChange = {this.handleChange} />
-                    </label>
-                    <label>
-                    GPA: 
-                    <input type = "number" value = {gpa} onChange = {this.handleChange} />
+            <form>
+                <label>
+                    First Name: 
+                    <input type="text" value={firstName} onChange={handleChange} />
                 </label>
-        </form>
-    </div>
-
+                <label>
+                    Last Name: 
+                    <input type="text" value={lastName} onChange={handleChange} />
+                </label>
+                <label>
+                    Image URL: 
+                    <input type="text" value={imageURL} onChange={handleChange} />
+                </label>
+                <label>
+                    Email: 
+                    <input type="text" value={email} onChange={handleChange} />
+                </label>
+                <label>
+                    GPA: 
+                    <input type="number" value={gpa} onChange={handleChange} />
+                </label>
+            </form>
+        </div>
     );
 }
 
-export default addNewStudent;*/
+export default AddNewStudent;
