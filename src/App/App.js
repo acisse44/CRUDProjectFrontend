35f29506cf5a7573dae2810bb4f8c2c86b373f1d
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-02 14:13:34 -0400

    add Provider to connect redux with react

--- src/App/App.js
+++ src/App/App.js
@@ -1,46 +1,50 @@
-import "../App/App.css"
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from '../Redux/store';
+import "../App/App.css";
 import Home from "../Pages/Home";
+import AddNewCampus from "../Pages/campuses/AddNewCampus";
 import Campuses from "../Pages/campuses/Campuses";
-import Students from "../Pages/students/Students";
 import Campus from "../Pages/campuses/Campus";
+import AddNewStudent from "../Pages/students/AddNewStudent";
+import Students from "../Pages/students/Students";
 import Student from "../Pages/students/Student";
-//import addNewCampus from "../Pages/campuses/addNewCampus"
-//import addNewStudent from "../Pages/students/addNewStudent"
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* Navigation */}
-        <nav className="nav-container">
-          <ul className="nav-links">
-            <li className="nav-item-left">
-            <Link to="/" id="home-link">Home</Link>
-            </li>
-            <li className="nav-item-right">
-            <Link to="/campuses" id="campuses-link">Campuses</Link>
-            </li>
-            <li className="nav-item-right">
-            <Link to="/students" id="students-link">Students</Link>
-            </li>
-          </ul>
-        </nav>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          {/* Navigation */}
+          <nav className="nav-container">
+            <ul className="nav-links">
+              <li className="nav-item-left">
+                <Link to="/" id="home-link">Home</Link>
+              </li>
+              <li className="nav-item-right">
+                <Link to="/campuses" id="campuses-link">Campuses</Link>
+              </li>
+              <li className="nav-item-right">
+                <Link to="/students" id="students-link">Students</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/campuses/*" element={<Campuses />} />
-          <Route path="/campuses/:id" element={<Campus />} />
-          <Route path="/students/*" element={<Students />} />
-          <Route path="/students/:id" element={<Student />} />
-         {//<Route path="/campuses/add" element={<addNewCampus/>} />
-          //<Route path="/students/add" element={<addNewStudent />} />
-        }
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/campuses/*" element={<Campuses />} />
+            <Route path="/campuses/:id" element={<Campus />} />
+            <Route path="/students/*" element={<Students />} />
+            <Route path="/students/:id" element={<Student />} />
+            <Route path="/campuses/add" element={<AddNewCampus/>} />
+            <Route path="/students/add" element={<AddNewStudent />} />
 
-        </Routes>
-      </div>
-    </Router>
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
