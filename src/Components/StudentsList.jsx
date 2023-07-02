b49640939ee97831e6831890077ce7988cb3c216
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-02 15:36:40 -0400

    fixed some logic and added functionality to add student button with redirect to form page

--- src/Components/StudentsList.jsx
+++ src/Components/StudentsList.jsx
@@ -1,30 +1,35 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import {  deleteAStudentThunk }  from "../Redux/students/students.actions"
-import "../App/App.css";
+import React from 'react';
+import '../App/App.css';
 
-function StudentsallStudents(props) {
-  const { allStudents } = props;
-  const dispatch = useDispatch();
-  const handleDelete = (id) => {
-    dispatch(deleteAStudentThunk(id));
-  }
-  
+function Students(props) {
+  const { allStudents, allCampuses, handleDelete } = props;
+  const campusesArray = Object.values(allCampuses);
+
+  const getCampusName = (campusId) => {
+    const campus = campusesArray.find((campus) => campus.id === campusId);
+    return campus ? campus.name : 'Unknown Campus';
+  };
+
+
   return (
     <div>
-      {allStudents.length === 0 ? (
+      {!allStudents || allStudents.length === 0 ? (
         <div>
           <div>Empty</div>
           <div>No students</div>
         </div>
       ) : (
-        <div>
-          {allStudents.map((allStudents) => (
-            <div id="student" key={allStudents.id}>
-              <img src={allStudents.imageUrl} alt={allStudents.firstName} id="studentImage" />
-              <div>{allStudents.firstName} {allStudents.lastName}</div>
-              <div>{allStudents.email}</div>
-              <button onClick={() => handleDelete(allStudents.id)}>Delete</button>
+        <div id="student-container">
+          {allStudents.map((student) => (
+            <div key={student.id} id="student">
+              <img src={student.imageUrl} alt={student.firstName} id="studentImage" />
+              <div id="student-name">{student.firstName} {student.lastName}</div>
+              <div id="student-email">{student.email}</div>
+              <div id="student-campus">{getCampusName(student.campusId)}</div>
+              <button className="button-edit">Edit</button>
+              <button className="button-delete" onClick={() => handleDelete(student.id)}>
+                Delete
+              </button>
             </div>
           ))}
         </div>
@@ -33,4 +38,4 @@ function StudentsallStudents(props) {
   );
 }
 
-export default StudentsallStudents;
+export default Students;
