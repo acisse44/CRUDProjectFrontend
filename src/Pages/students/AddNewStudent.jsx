2400221757001229837ca9aaba43679c45e916b1
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-04 11:42:03 -0400

    made some changes

--- src/Pages/students/AddNewStudent.jsx
+++ src/Pages/students/AddNewStudent.jsx
@@ -2,65 +2,44 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { addStudentThunk } from '../../Redux/students/students.actions';
-import Students from '../students/Students';
 import "../../CSS/campusForm.css"
-
 
 function AddNewStudent() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
-  const [email, setEmail] = useState('');
-  const [gpa, setGpa] = useState('');
-  const [campusId, setCampusId] = useState('');
-  const [submitted, setSubmitted] = useState(false);
   const allCampuses = useSelector((state) => state.campuses.allCampuses);
+  const [studentData, setStudentData] = useState({
+    firstName: '',
+    lastName: '',
+    imageUrl: '',
+    email: '',
+    gpa: '',
+    campusId: '',
+  });
+  const [submitted, setSubmitted] = useState(false);
 
-
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
-
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
-  };
-
-  const handleImageUrlChange = (event) => {
-    setImageUrl(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleGpaChange = (event) => {
-    setGpa(event.target.value);
-  };
-
-  const handleCampusChange = (event) => {
-    setCampusId(event.target.value);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setStudentData((prevData) => ({ ...prevData, [name]: value }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const studentData = {
-      firstName,
-      lastName,
-      imageUrl,
-      email,
-      gpa,
-      campusId,
-    };
+    if (!studentData.firstName || !studentData.lastName || 
+        !studentData.email || !studentData.gpa || !studentData.campusId) {
+      alert('Please fill out all required fields.');
+      return;
+    }
     dispatch(addStudentThunk(studentData));
 
-    setFirstName('');
-    setLastName('');
-    setImageUrl('');
-    setEmail('');
-    setGpa('');
-    setCampusId('');
+    setStudentData({
+      firstName: '',
+      lastName: '',
+      imageUrl: '',
+      email: '',
+      gpa: '',
+      campusId: '',
+    });
     setSubmitted(true);
   };
 
@@ -71,42 +50,91 @@ function AddNewStudent() {
 
   return (
     <div>
-      <h1 class="form-title">Add A New Student</h1>
-      <form class="form" onSubmit={handleSubmit}>
-        <div class="input">
-          <label class="input-label">
+      <h1 className="form-title">Add A New Student</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input">
+          <label htmlFor="firstName" className="input-label">
             First Name:
-            <input class="input-field" type="text" value={firstName} onChange={handleFirstNameChange} />
+            <input
+              id="firstName"
+              className="input-field"
+              type="text"
+              name="firstName"
+              value={studentData.firstName}
+              onChange={handleChange}
+              placeholder="Enter your first name"
+            />
           </label>
         </div>
-        <div class="input">
-          <label class="input-label">
+        <div className="input">
+          <label htmlFor="lastName" className="input-label">
             Last Name:
-            <input class="input-field" type="text" value={lastName} onChange={handleLastNameChange} />
+            <input
+              id="lastName"
+              className="input-field"
+              type="text"
+              name="lastName"
+              value={studentData.lastName}
+              onChange={handleChange}
+              placeholder="Enter your last name"
+
+            />
           </label>
-        </div >
-        <div class="input">
-          <label class="input-label">
+        </div>
+        <div className="input">
+          <label htmlFor="imageUrl" className="input-label">
             Image URL:
-            <input class="input-field" type="text" value={imageUrl} onChange={handleImageUrlChange} />
+            <input
+              id="imageUrl"
+              className="input-field"
+              type="text"
+              name="imageUrl"
+              value={studentData.imageUrl}
+              onChange={handleChange}
+              placeholder="Enter your image URL"
+
+            />
           </label>
         </div>
-        <div class="input">
-          <label class="input-label">
+        <div className="input">
+          <label htmlFor="email" className="input-label">
             Email:
-            <input class="input-field" type="text" value={email} onChange={handleEmailChange} />
+            <input
+              id="email"
+              className="input-field"
+              type="text"
+              name="email"
+              value={studentData.email}
+              onChange={handleChange}
+              placeholder="Enter your email address"
+
+            />
           </label>
         </div>
-        <div class="input">
-          <label class="input-label">
+        <div className="input">
+          <label htmlFor="gpa" className="input-label">
             GPA:
-            <input class="input-field" type="text" value={gpa} onChange={handleGpaChange} />
+            <input
+              id="gpa"
+              className="input-field"
+              type="text"
+              name="gpa"
+              value={studentData.gpa}
+              onChange={handleChange}
+              placeholder="Enter your GPA"
+            />
           </label>
         </div>
-        <div class="input">
-          <label class="input-selectlabel">
+        <div className="input">
+          <label htmlFor="campusId" className="input-selectlabel">
             Campus:
-            <select class="input-select" value={campusId} onChange={handleCampusChange}>
+            <select
+              id="campusId"
+              className="input-select"
+              name="campusId"
+              value={studentData.campusId}
+              onChange={handleChange}
+            >
               <option value="">Select a campus</option>
               {allCampuses.map((campus) => (
                 <option key={campus.id} value={campus.id}>
@@ -116,7 +144,7 @@ function AddNewStudent() {
             </select>
           </label>
         </div>
-        <button class="button-submit" type="submit">Submit</button>
+        <button className="button-submit" type="submit">Submit</button>
       </form>
     </div>
   );
